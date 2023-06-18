cc7ef38c797770f173a32791686fa03381d73791
sui <1202ayasnppk@gmail.com>
2023-06-19 07:33:54 +0900

    TaskCollectionクラスの作成まで。大文字と小文字で怒られてるけど、どこが悪いのかわからん

--- ts/index.ts
+++ ts/index.ts
@@ -1,8 +1,11 @@
 import { EventListener } from "./EventListener";
-import { Task } from "./task";
+import { TaskCollection } from "./TaskCollection";
+import { Task } from './Task'
 
 class Application {
   private readonly eventLister = new EventListener()
+  private readonly taskCollection = new TaskCollection()
+
   start() {
     const createForm = document.getElementById('createForm') as HTMLElement
 
@@ -16,7 +19,9 @@ class Application {
     if(!titleInput.value) return
 
     const task = new Task({title: titleInput.value})
-    console.log(task);
+    this.taskCollection.add(task)
+    console.log(this.taskCollection);
+    
   }
 }
 
--- ts/task.ts
+++ ts/task.ts
@@ -1,7 +1,28 @@
+import { v4 as uuid } from 'uuid'
+
+// 列挙型 あまり使うべきではない理由もあるらしい
+// as constでタプルにしてからtypeofしたほうが手堅い
+// enum Status {
+//   Todo = 'TODO',
+//   Doing = 'DOING',
+//   Done = 'DONE'
+// }
+
+export const statusMap =  {
+  todo: 'TODO',
+  doing: 'DOING',
+  done: 'DONE'
+} as const
+
+export type Status = typeof statusMap[keyof typeof statusMap]
 export class Task {
+  readonly id:any
   title:any
+  status:any
 
   constructor(properties: {title: string}) {
+    this.id = uuid()
     this.title = properties.title
+    this.status = statusMap.todo
   }
 }
